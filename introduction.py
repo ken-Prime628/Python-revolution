6ca568b44dfaf8e8846bec846e55e8424a9f6df0
ken-prime628 <kennedywaturi@gmail.com>
2026-02-03 10:26:29 +0300

    initial commit

---
+++ introduction.py
@@ -0,0 +1,7 @@
+#Print is a method used to display an output
+
+
+print("Welcome to Python Programming")
+print("Kennedy")
+print(100)
+print(20*3)
---
+++ variables.py
@@ -0,0 +1,16 @@
+#Variable is a container used to store a value
+
+school = "eMobilis"
+firstname ="Kennedy"
+lastname ="Marcus"
+num1 =36
+num2 =42
+language ="python"
+
+print(school)
+print(firstname,lastname)
+print(num1+num2)
+print(language,"is a web programming language")
+print("The sum of",num1,"and",num2,"is ",num1+num2)
+
+
